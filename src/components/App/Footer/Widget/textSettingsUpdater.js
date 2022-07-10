eaa116b8b1baef36cc216076049e912e8407d42d
Jack Mildren <jmildren1@gmail.com>
2022-07-10 04:27:04 +0100

    Removing Unnecessary Comments

--- src/components/App/Footer/Widget/textSettingsUpdater.js
+++ src/components/App/Footer/Widget/textSettingsUpdater.js
@@ -23,16 +23,11 @@ export const textSettingsUpdater = createSlice({
             state.value.fontFamily = action.payload
         },
         resetAll: (state) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.value = {}
         },
     },
 })
 
-// Action creators are generated for each case reducer function
 export const { resetAll, setLineHeight, setFontSize, setWordSpacing, setFontFamily } = textSettingsUpdater.actions
 
 export default textSettingsUpdater.reducer
--- src/components/App/contactDB.js
+++ src/components/App/contactDB.js
@@ -12,10 +12,6 @@ export const contactDB = createSlice({
 
     reducers: {
         resetAll: (state) => {
-            // Redux Toolkit allows us to write "mutating" logic in reducers. It
-            // doesn't actually mutate the state because it uses the Immer library,
-            // which detects changes to a "draft state" and produces a brand new
-            // immutable state based off those changes
             state.value = {}
         },
         getFromFile: (state) => {
@@ -28,7 +24,6 @@ export const contactDB = createSlice({
     },
 })
 
-// Action creators are generated for each case reducer function
 export const { resetAll, } = contactDB.actions
 
 export default contactDB.reducer
