ce7fa46ea3d776c8d6173254df1da4219ba7d548
Jack Mildren <jmildren1@gmail.com>
2022-07-11 16:06:12 +0100

    Removing konva and generating shapes with canvas instead

--- src/components/App/Main/Games/ShapeGame.js
+++ src/components/App/Main/Games/ShapeGame.js
@@ -1,85 +1,81 @@
-import { Stage, Layer, Shape, Text, Circle, RegularPolygon } from "react-konva"
-import Konva from "konva";
-import { startTransition, useEffect, useRef, useState } from "react"
+import { useState, useRef, useEffect } from "react"
+import { isEqual } from "underscore";
 
 export default function ShapeGame () {
-    // const [shapes, setShapes] = useState([]);
-    // const [rnd, setRnd] = useState(null);
 
-    // const shapes = [
-    //     {
-    //         attrs: {
-    //             x : 50,
-    //             y : 50,
-    //             radius : 50,
-    //             fill : "green"
-    //         },
-    //         className : "Circle"
-    //     }
-    // ]
+    const canvasRef = useRef(null)
 
-    // useEffect(() => {
-    //     const layer1 = questionLayer.current
-
-    //     let newShapes = [...shapes]
-
-    //     for (let i = 0; i < 5; i++) {
-    //         const newShape = new Konva.RegularPolygon({
-    //             x: window.innerWidth * 0.1 * (i+1) * 2,
-    //             y: window.innerHeight/2,
-    //             sides: Math.ceil(Math.random() * 5 + 3),
-    //             radius: 50,
-    //             fill: "green"
-    //         });
-    //         newShapes.push(newShape);
-    //     };
-        
-    //     // layer1.add(newShape);
-    //     // layer1.batchDraw();
-    //     console.log(newShapes)
-    //     setShapes(newShapes)
-    //     // TODO: use the sides property in Konva.RegularPolygon to generate polygons
-    // }, [rnd]);
-
-    function generateShapes() {
-        return [...Array(5)].map((_, i) => ({
-            id: i.toString(),
-            x: window.innerWidth * 0.1 * (i+1) * 2,
-            y: window.innerHeight/2,
-            sides: Math.ceil(Math.random() * 5 + 3)
-        }))
+    const regularpolygon = (ctx, x, y, radius, sides) => {
+        if (sides < 3) return;
+        ctx.beginPath();
+        let a = ((Math.PI * 2)/sides);
+        ctx.translate(x,y);
+        ctx.moveTo(radius,0);
+        for (let i = 1; i < sides; i++) {
+            ctx.lineTo(radius*Math.cos(a*i),radius*Math.sin(a*i));
+        }
+        ctx.closePath();
     }
 
-    const INITIAL_STATE = generateShapes();
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d') 
+        
+        const colours = ["blue", "red", "yellow", "green", "orange", "purple"]
 
-    const [shapes, setShapes] = useState(INITIAL_STATE);
+        let shapeList = [];
 
-    const questionLayer = useRef();
+        for (let i = 0; i < 5; i++) {
+            const x = 70
+            const y = 70
+            let sides;
+            let colour;
+            let newShape
+
+            function generateShapeRandoms() {
+                while (true) {
+                    sides = Math.floor(Math.random() * 6) + 3
+
+                    colour = colours[Math.floor(Math.random() * colours.length)]
+
+                    newShape = [sides, colour]
+
+                    // Check for duplicates
+                    let valid = true;
+                    for (let i = 0; i < shapeList.length; i++) {
+                        if (isEqual(shapeList[i], newShape)) {
+                            valid = false;
+                            break;
+                        }  
+                    }
+
+                    if (valid) {
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+                return newShape
+            }
+            
+            newShape = generateShapeRandoms()
+
+            regularpolygon(ctx, x, y, 50, sides)
+
+            ctx.fillStyle = colour;
+            ctx.fill();
+            ctx.translate(x,-y);
+
+            shapeList.push(newShape)
+    }
+        
+
+    }, [regularpolygon]);
 
     return (
         <div className="ShapeGame">
-            <Stage width={window.innerWidth} height={window.innerHeight}>
-                <Layer ref={questionLayer}>
-                    {/* <RegularPolygon
-                        x={100}
-                        y={100}
-                        sides={9}
-                        fill="green"
-                        radius={50}
-                    /> */}
-                    {shapes.map((shape, i) => {
-                        <RegularPolygon
-                            key={shape.id}
-                            id={shape.id}
-                            x={shape.x}
-                            y={shape.y}
-                            sides={shape.sides}
-                            fill="green"
-                            radius={50}
-                        />
-                    })}
-                </Layer>
-            </Stage>
+            <canvas ref={canvasRef} width={window.innerWidth} height={window.innerHeight}
+            />
         </div>
     )
 }
