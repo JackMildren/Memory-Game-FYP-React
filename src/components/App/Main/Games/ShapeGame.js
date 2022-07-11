2d04e4b4ddcb1e0ca74a54872b9a17af5df07a60
Jack Mildren <jmildren1@gmail.com>
2022-07-11 21:32:56 +0100

    adding more shapes to the shape generator

--- src/components/App/Main/Games/ShapeGame.js
+++ src/components/App/Main/Games/ShapeGame.js
@@ -5,15 +5,24 @@ export default function ShapeGame () {
 
     const canvasRef = useRef(null)
 
-    const regularpolygon = (ctx, x, y, radius, sides) => {
+    const regularpolygon = (ctx, x, y, radius, sides, isDiamond = false) => {
         if (sides < 3) return;
         ctx.beginPath();
         let a = ((Math.PI * 2)/sides);
         ctx.translate(x,y);
+        const rotateVal = (90 * Math.PI / 180)
+        !isDiamond ? ctx.rotate(-rotateVal) : ctx.rotate(-rotateVal/2);
         ctx.moveTo(radius,0);
         for (let i = 1; i < sides; i++) {
             ctx.lineTo(radius*Math.cos(a*i),radius*Math.sin(a*i));
         }
+        !isDiamond ? ctx.rotate(rotateVal) : ctx.rotate(rotateVal/2);
+        ctx.closePath();
+    }
+
+    const circle = (ctx, x, y, radius) => {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
         ctx.closePath();
     }
 
@@ -34,7 +43,7 @@ export default function ShapeGame () {
 
             function generateShapeRandoms() {
                 while (true) {
-                    sides = Math.floor(Math.random() * 6) + 3
+                    sides = Math.floor(Math.random() * 7)
 
                     colour = colours[Math.floor(Math.random() * colours.length)]
 
@@ -49,6 +58,8 @@ export default function ShapeGame () {
                         }  
                     }
 
+                    sides === 2 && (valid = false);
+
                     if (valid) {
                         break;
                     } else {
@@ -60,17 +71,33 @@ export default function ShapeGame () {
             
             newShape = generateShapeRandoms()
 
-            regularpolygon(ctx, x, y, 50, sides)
+            switch (sides) {
+                case 0:
+                    circle(ctx, x, y, 50);
+                    ctx.translate(x*2, 0);
+                    break;
+                case 1:
+                    regularpolygon(ctx, x, y, 50, 4, true);
+                    ctx.translate(x,-y);
+                    break;
+                case 2:
+                    break;
+                default:
+                    regularpolygon(ctx, x, y, 50, sides);
+                    ctx.translate(x,-y);
+                    break;
+            }
 
             ctx.fillStyle = colour;
             ctx.fill();
-            ctx.translate(x,-y);
+            ctx.stokeStyle = "black"
+            ctx.stroke()
 
             shapeList.push(newShape)
     }
         
 
-    }, [regularpolygon]);
+    }, []);
 
     return (
         <div className="ShapeGame">
