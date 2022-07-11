0483c99617bead9e9fdbae64bf7d87ebda9a7ddd
Jack Mildren <jmildren1@gmail.com>
2022-07-11 02:33:38 +0100

    Setting up a basic Konva scene

--- src/components/App/Main/Games/ShapeGame.js
+++ src/components/App/Main/Games/ShapeGame.js
@@ -1,10 +1,39 @@
-
+import { Stage, Layer, Shape, Text, Circle, RegularPolygon } from "react-konva"
+import Konva from "konva";
+import { useEffect, useRef } from "react"
 
 export default function ShapeGame () {
+    const questionLayer = useRef();
+
+    const shapes = [
+        {
+            attrs: {
+                x : 50,
+                y : 50,
+                radius : 50,
+                fill : "green"
+            },
+            className : "Circle"
+        }
+    ]
+
+    useEffect(() => {
+        // const layer1 = questionLayer.current
+        // const newShape = new Konva.Shape(shapes)
+
+        // layer1.add(newShape);
+        // layer1.batchDraw();
+
+        // console.log(layer1)
+    })
+
     return (
         <div className="ShapeGame">
-            <h2>Shape Game!</h2>
-
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer ref={questionLayer}>
+                    <Circle x={50} y={50} radius={50} fill="green"/>
+                </Layer>
+            </Stage>
         </div>
     )
 }
