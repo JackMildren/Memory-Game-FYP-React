74873d4d51e2945897e461cdb7e16d3f78e534a0
Jack Mildren <jmildren1@gmail.com>
2022-07-12 23:58:05 +0100

    Adding Answer layer and implimenting selection

--- src/components/App/Main/Games/ShapeGame.js
+++ src/components/App/Main/Games/ShapeGame.js
@@ -1,38 +1,99 @@
 import { Stage, Layer, RegularPolygon } from "react-konva";
-import Konva from "konva";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { isCompositeComponent } from "react-dom/test-utils";
 
 export default function ShapeGame() {
-  function generateShapes() {
-    return [...Array(5)].map((_, i) => ({
-      id: i.toString(),
-      x: window.innerWidth * 0.2 * (i + 1),
-      y: window.innerHeight / 2,
-      sides: Math.ceil(Math.random() * 5 + 3),
-    }));
-  }
+    let stageRef = useRef(null);
+    let questionLayerRef = useRef(null);
+    let answerLayerRef = useRef(null);
 
-  const INITIAL_STATE = generateShapes();
+    const colourList = ['hotpink', 'blue', 'green', 'yellow', 'orange', 'purple'];
+    const questionShapeCount = 5;
+    const answerShapeCount = 4;
+    
+    const [qShapes, setQShapes] = useState(null);
+    const [aShapes, setAShapes] = useState(null);  
+    
+    const [selectedShape, setSelectedShape] = useState(answerShapeCount + 1);
 
-  const [shapes, setShapes] = useState(INITIAL_STATE);
+    useEffect(() => {
+        stageRef = stageRef.current
 
-  return (
+        if (!qShapes) {
+            setQShapes(generateShapes(true))
+            setAShapes(generateShapes(false))
+        }
+    }, [selectedShape]);
+
+    function generateShapes(questionLayer) {
+        const heightAdjust = (questionLayer ? 0.25 : 0.75);
+        const shapeCount = (questionLayer ? questionShapeCount : answerShapeCount);
+
+        const border = stageRef.attrs.width * 0.3;
+        const width = stageRef.attrs.width - border;
+        const height = stageRef.attrs.height;
+
+        return [...Array(shapeCount)].map((_, i) => ({
+            id: i.toString(),
+            x: Math.floor(width / (shapeCount - 1) * i) + (border / 2),
+            y: Math.floor(height * heightAdjust),
+            sides: Math.ceil(Math.random() * 3 + 3),
+            color: colourList[Math.floor(Math.random() * colourList.length)],
+            radius: Math.min(80, width),
+            strokeWidth: (!questionLayer && (selectedShape === i)) ? 5 : 1,
+            stroke: (!questionLayer && (selectedShape === i)) ? "yellow" : "black",
+        }));
+    }
+
+    function selectShape(shapeNo) {
+        setSelectedShape(shapeNo);
+
+        const tempShapes = [...aShapes];
+        for (let i=0; i < tempShapes.length; i++) {
+            tempShapes[i].strokeWidth = (shapeNo === i.toString()) ? 10 : 1;
+        }
+        return tempShapes;
+    }
+
+    function handleClick(shape) {
+        setAShapes(selectShape(shape.id))
+    }
+
+    return (
     <div className="ShapeGame">
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          {shapes.map((shape, i) => (
-            <RegularPolygon
-              key={shape.id}
-              id={shape.id}
-              x={shape.x}
-              y={shape.y}
-              sides={shape.sides}
-              fill="green"
-              radius={50}
-            />
-          ))}
-        </Layer>
-      </Stage>
+        <Stage ref={stageRef} width={window.innerWidth - 16} height={window.innerHeight * 0.75}>
+            <Layer ref={questionLayerRef}>
+                {qShapes && qShapes.map((shape) => (
+                <RegularPolygon
+                    key={shape.id}
+                    id={shape.id}
+                    x={shape.x}
+                    y={shape.y}
+                    sides={shape.sides}
+                    fill={shape.color}
+                    radius={shape.radius}
+                    strokeWidth={shape.strokeWidth}
+                    stroke={shape.stroke}
+                />
+                ))}
+            </Layer>
+            <Layer ref={answerLayerRef}>
+                {aShapes && aShapes.map((shape) => (
+                <RegularPolygon
+                    key={shape.id}
+                    id={shape.id}
+                    x={shape.x}
+                    y={shape.y}
+                    sides={shape.sides}
+                    fill={shape.color}
+                    radius={shape.radius}
+                    strokeWidth={shape.strokeWidth}
+                    stroke={shape.stroke}
+                    onClick={() => {handleClick(shape)}}
+                />
+                ))}
+            </Layer>
+        </Stage>
     </div>
-  );
+    );
 }
