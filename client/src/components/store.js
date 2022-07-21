59f2ab669083e980d462e20505e12cefbc7f933a
Jack Mildren <jmildren1@gmail.com>
2022-07-21 11:43:47 +0100

    fixing bug after removing contactdb

--- client/src/components/store.js
+++ client/src/components/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import contactDB from "./App/contactDB";
 import textSettingsUpdater from "./App/Footer/Widget/textSettingsUpdater";
 
 export default configureStore({
   reducer: {
     textSettings: textSettingsUpdater,
-    contactDB: contactDB,
   },
 });
