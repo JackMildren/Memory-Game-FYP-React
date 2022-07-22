b1d8d1656d5438bdb945ed91da55c19fdcc38066
Jack Mildren <jmildren1@gmail.com>
2022-07-22 01:44:49 +0100

    title fix

--- client/src/components/App/Header/Title.js
+++ client/src/components/App/Header/Title.js
@@ -4,7 +4,7 @@ export default class Title extends Component {
   render() {
     return (
       <div className="Title">
-        <h1>Memorize!</h1>
+        <h1>Memory Game FYP</h1>
       </div>
     );
   }
